4dcccf46fc8af0dfce1d4c6e3a20c1c57d97f1eb
mohsin902 <mshabbir@egenienext.com>
2019-10-02 00:45:54 +0500

    some layout changes

--- App.js
+++ App.js
@@ -15,8 +15,7 @@ import {
   Text,
   StatusBar,
   TouchableOpacity ,
-  Button,
-  Dimensions
+  Button
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {
@@ -29,18 +28,37 @@ import {
 
 export default class App extends Component {
   render(){
+    let numbers = []
+    let numArr = [[1,2,3] , [4,5,6] , [7,8,9] , ['+/_',0,'.']]
+    for(var i=0;i<4;i++){
+      let row = []
+      for(var j=0;j<3;j++){
+        row.push(<TouchableOpacity style = {styles.btn}>
+          <Text style= {styles.textbtn}>{numArr[i][j]}</Text>
+        </TouchableOpacity>)
+      }
+
+      numbers.push(<View style = {styles.row}>{row}</View>)
+    }
   return (
       <View style={styles.rootContainer}>
-    <View style={styles.displayContainer}></View>
-    <View style = {styles.midContainer}>
-    <Text style = {{paddingLeft : 12}}>HISTORY </Text>
-      <Icon name="delete" size={28} color="#1e90ff" style = {{right: 0}} />
+    <View style={styles.displayContainer}>
+      <Text style = {styles.text}> 11 * 5</Text>
     </View>
-    <View style={styles.inputContainer}>
-      <View style={styles.button}>
+    <View style = {styles.midContainer}>
+      <Text style = {styles.caltext}>111</Text>
+    </View>
+  
+    
+    <View style={styles.buttons}>
+      <View style={styles.numbers}>
+       {numbers}
       </View>
-      <View>
-
+      <View style={styles.operations}>
+      <Button title = '+' />
+      <Button title = '*' />
+      <Button title = '/' />
+      <Button title = '=' />
       </View>
     </View>
 </View>
@@ -54,22 +72,55 @@ const styles = StyleSheet.create({
 },
 
 displayContainer: {
-    flex: 4,
-    backgroundColor: '#ffffff'
+    flex: 2,
+    backgroundColor: 'white'
+},
+text : {
+    fontSize : 30,
+    color : 'black'
 },
 midContainer: {
   flex : 1,
-  flexDirection : 'row',
-  alignItems : 'center',
-  backgroundColor : '#f5f5f5'
+  backgroundColor : 'white'
 },
 inputContainer: {
     flex: 8,
     backgroundColor: '#ffffff',
     flexDirection : 'row',
 },
-button: {
+buttons: {
+  flex: 7,
+  flexDirection : 'row'
+},
+numbers:{
+  flex : 3 , 
+  backgroundColor : 'white'
+},
+row : {
+  flexDirection : 'row',
+  flex : 1,
+  justifyContent : 'space-around',
+  alignItems : 'center'
+},
+caltext : {
+  fontSize : 24 , 
+  color : 'black'
+},
+operations : {
+  flex : 1 , 
+  backgroundColor : 'black',
+  justifyContent : 'space-around',
+  alignItems : 'stretch'
 
+},
+btn : {
+  flex : 1 ,
+  alignItems: 'center',
+  alignSelf : 'stretch',
+  justifyContent : 'center'
+},
+textbtn : {
+  fontSize : 25
 }
 });
 
